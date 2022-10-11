854ea8802f5488c5f12dbcfa40ab4c68226a8192
Igor Lima <igorlimagn@gmail.com>
2022-10-10 21:36:27 -0300

    @igorgll [JOptionPane Ex]

---
+++ Aula7Ex2.java
@@ -0,0 +1,48 @@
+import javax.swing.JOptionPane;
+
+public class Aula7Ex2 {
+    public static void menu() {
+        double a, b, c, sp;
+        int itemMenu;
+        String tipo = "";
+        String menu = "MENU";
+        menu += "\n1 Executar \n2 Finalizar";
+        do {
+            String strItem = JOptionPane.showInputDialog(null, menu);
+            itemMenu = strItem.charAt(0);
+            switch (itemMenu) {
+                case '1':
+                    a = Double.parseDouble(JOptionPane.showInputDialog(null, "Lado A:"));
+                    b = Double.parseDouble(JOptionPane.showInputDialog(null, "Lado B:"));
+                    c = Double.parseDouble(JOptionPane.showInputDialog(null, "LAdo C:"));
+                    if (a >= b + c || b >= a + c || c >= a + b || a <= 0 || b <= 0 || c <= 0) {
+                        String saida = "Erro! Lados inválidos!";
+                        JOptionPane.showMessageDialog(null, saida);
+                        break;
+                    } else {
+                        sp = (a + b + c) / 2;
+
+                        if (a == b && b == c) {
+                            tipo = "Equilátero";
+                        } else if (a != b && a != c && b != c) {
+                            tipo = "Escaleno";
+                        } else {
+                            tipo = "Isósceles";
+                        }
+                    }
+                    JOptionPane.showMessageDialog(null, tipo + " " + sp);
+                    break;
+                case '2':
+                    JOptionPane.showMessageDialog(null, "Programa Finalizado!");
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção Inválida!");
+            }
+        } while (true);
+    }
+
+    public static void main(String[] args) {
+        menu();
+    }
+}
